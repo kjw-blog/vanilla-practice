2b525bb287a25add3d621d945e27bad4c08b99c6
kjw-blog <rkdwjddnr11@naver.com>
2023-12-22 23:59:49 +0900

    feat: js 기초
    - boolean, null, undefined

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-let a = 5;
-let b = 2;
+// boolean
+// true, false 2가지 값의 타입
 
-// var는 메모 생략
+// false와 null, undefined는 다르며, "true" 나 "false"처럼 사용할 수 없음 해당 값음 boolean값이 아니라 string임
 
-let myName = 'jeong wook kang'; // camel case : JS에서 주로 씀
-const my_name = 'jeong wook kang'; // snake case : python에서 주로 씀 python에서는 const나 let같은 키워드가 필요없다함
+console.log(typeof 'true'); // string
+console.log(typeof true); // boolean
 
-console.log(5 + 2);
-console.log(5 * 2);
-console.log(5 / 2);
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log('hello ' + myName);
-
-myName = '강정욱';
-// let은 값을 재할당하는것이 가능하다.
-
-// my_name = '강정욱';
-// const는 안됨 오류발생
-
-console.log('your new name is ' + myName);
+// undefined : 변수는 있는데 값이 아직 할당되지 않은 것
+// null : 변수안에 비어있는 값이 있다는 것
