8e79fe750243882bd9f091846b58f808d98be611
kjw-blog <rkdwjddnr11@naver.com>
2023-12-22 23:44:45 +0900

    feat: js 기초
    - 변수 선언 방식과 변수명 관례

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert('hi');
+const a = 5;
+const b = 2;
+
+const myName = 'jeong wook kang'; // camel case : JS에서 주로 씀
+const my_name = 'jeong wook kang'; // snake case : python에서 주로 씀 python에서는 const나 let같은 키워드가 필요없다함
+
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('hello ' + myName);
