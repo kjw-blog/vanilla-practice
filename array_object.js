bdc43e811a01351c28aca27645d59a52853e8e58
kjw-blog <rkdwjddnr11@naver.com>
2023-12-25 23:59:51 +0900

    feat: js 기초
    - object

--- array_object.js
+++ array_object.js
@@ -1,3 +1,5 @@
+// array
+
 const mon = 'mon';
 const tue = 'tue';
 const wed = 'wed';
@@ -19,3 +21,36 @@ console.log(daysOfWeekArray[4]);
 
 // 배열의 마지막에 값을 추가한다.
 daysOfWeekArray.push('sun');
+
+// object
+
+// 변수 4개의 값이 player 하나에 대한 값이다.
+const playerName = 'kjw';
+const playerPoint = 10000;
+const playerAge = 28;
+const playerFat = 'little bit';
+
+// array의 경우 값을 묶어주지만, 해당 값이 어떤 값인지 알 수 없다.
+const playerArray = ['kjw', 10000, 28, 'little bit'];
+
+// object를 사용하면 값을 묶고, 해당 값에 대한 식별자를 지정해 줄 수 있다.
+const playerObject = {
+  name: 'kjw',
+  point: 10000,
+  age: 28,
+  fat: 'little bit',
+};
+
+//  object의 값 전체 조회
+console.log(playerObject);
+
+// object의 하나의 property 조회
+console.log(playerObject.name);
+
+// object의 하나의 property값 변경
+playerObject.age = 29;
+
+// object에 property 추가
+playerObject.hobby = '방탈출';
+
+console.log(playerObject);
